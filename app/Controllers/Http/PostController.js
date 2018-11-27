49e2669909accfd604d014d75da7826b3c130b6a
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-27 14:22:59 -0800

    (feat) Create main template and use in posts route

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -2,8 +2,16 @@
 
 class PostController {
   async index({ view }) {
+    const posts = [
+      { title: 'Post One', body: 'This is post one' },
+      { title: 'Post Two', body: 'This is post two' },
+      { title: 'Post Three', body: 'This is post three' },
+      { title: 'Post Four', body: 'This is post four' }
+    ]
+
     return view.render('posts.index', {
-      title: 'Latest Posts'
+      title: 'Latest Posts',
+      posts
     })
   }
 }
