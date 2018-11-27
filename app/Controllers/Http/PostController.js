bfe186b7868ffd48abb5812fdc59e46d457c2b91
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-27 15:02:27 -0800

    (feat) Links on route /posts link to pages with the corresponding post

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -11,6 +11,14 @@ class PostController {
       posts: posts.toJSON()
     })
   }
+
+  async details({ params, view }) {
+    const post = await Post.find(params.id)
+
+    return view.render('posts.details', {
+      post
+    })
+  }
 }
 
 module.exports = PostController
