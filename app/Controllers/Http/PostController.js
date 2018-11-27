6389aabc0e6bb9927a0b9a3c39feaa3cebed21c0
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-27 14:56:05 -0800

    (refactor) Remove commented out code

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -4,13 +4,6 @@ const Post = use('App/Models/Post')
 
 class PostController {
   async index({ view }) {
-    // const posts = [
-    //   { title: 'Post One', body: 'This is post one' },
-    //   { title: 'Post Two', body: 'This is post two' },
-    //   { title: 'Post Three', body: 'This is post three' },
-    //   { title: 'Post Four', body: 'This is post four' }
-    // ]
-
     const posts = await Post.all();
 
     return view.render('posts.index', {
