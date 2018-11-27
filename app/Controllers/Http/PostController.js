a8e9500f5bf0784f3bdd0ff42931a0e620c04ef2
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-27 14:55:32 -0800

    (feat) Get posts from database
    Was using hardcoded data

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,17 +1,21 @@
 'use strict'
 
+const Post = use('App/Models/Post')
+
 class PostController {
   async index({ view }) {
-    const posts = [
-      { title: 'Post One', body: 'This is post one' },
-      { title: 'Post Two', body: 'This is post two' },
-      { title: 'Post Three', body: 'This is post three' },
-      { title: 'Post Four', body: 'This is post four' }
-    ]
+    // const posts = [
+    //   { title: 'Post One', body: 'This is post one' },
+    //   { title: 'Post Two', body: 'This is post two' },
+    //   { title: 'Post Three', body: 'This is post three' },
+    //   { title: 'Post Four', body: 'This is post four' }
+    // ]
+
+    const posts = await Post.all();
 
     return view.render('posts.index', {
       title: 'Latest Posts',
-      posts
+      posts: posts.toJSON()
     })
   }
 }
